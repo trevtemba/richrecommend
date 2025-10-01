4cf7a7c67525ecc1965ea2e09aa413240300ddac
Temba <drxpnotlox@gmail.com>
2025-10-01 00:10:20 -0400

    Weaved normalizer agent's call into orchestrator and declared it's function

--- internal/agents/normalizer/normalizer.go
+++ internal/agents/normalizer/normalizer.go
@@ -1,1 +1,12 @@
 package normalizer
+
+import (
+	"github.com/trevtemba/richrecommend/internal/logger"
+	"github.com/trevtemba/richrecommend/internal/models"
+)
+
+func NormalizeProducts(loadedData models.ScraperResponse, includedFields []string, requestId string) (models.NormalizerResponse, error) {
+	logger.Log(logger.LogTypeAgentFinish, logger.LevelInfo, "Normalizer agent started", "request_id", requestId)
+	var normalizedRecommendations models.NormalizerResponse
+	return normalizedRecommendations, nil
+}
